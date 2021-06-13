cfdaff6bb1e0332bcc09983a5370bc2a4902b59d
Jere <jerekoivisto7@gmail.com>
2021-06-13 14:28:49 +0300

    Painojen nostaminen onnistuu atm - Jere

--- Assets/Scripts/WeightLiftScript.cs
+++ Assets/Scripts/WeightLiftScript.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class WeightLiftScript : MonoBehaviour
 {
-    private bool idle;
+    public GameObject Hahmo;
+
+    private bool idle = true;
     private bool nosto;
+    private bool nosto2;
+    private bool nosto3;
     private bool nosto_aloitus;
     private bool nosto_aloitus2;
+
+    private int clickCounter = 0;
 
     private Animator anim;
 
@@ -19,11 +25,19 @@ public class WeightLiftScript : MonoBehaviour
     private void Update()
     {
         CheckInput();
+        UpdateAnimations();
+
+        if (clickCounter == 16)
+        {
+
+        }
     }
 
     private void UpdateAnimations()
     {
         anim.SetBool("nosto", nosto);
+        anim.SetBool("nosto2", nosto2);
+        anim.SetBool("nosto3", nosto3);
         anim.SetBool("nosto_aloitus", nosto_aloitus);
         anim.SetBool("nosto_aloitus2", nosto_aloitus2);
     }
@@ -33,20 +47,49 @@ public class WeightLiftScript : MonoBehaviour
         if (Input.GetButtonDown("Nosto"))
         {
             NostoFunction();
+            clickCounter++;
             Debug.Log("aikoo nostaa painoja");
             //kato miten require certain amount of clicks to perform action unity
+            //Miten animaatioon saa startin ja endin
         }
     }
 
     private void NostoFunction()
     {
-        if (idle == false && nosto == true && nosto_aloitus == false && nosto_aloitus2 == false)
+        if (clickCounter <= 1)
         {
-
+            idle = false;
+            nosto_aloitus = true;
+            Debug.Log("on function sisällä");
         }
-        else
+        else if(clickCounter <= 2)
         {
-
+            idle = false;
+            nosto_aloitus = false;
+            nosto_aloitus2 = true;
+            Debug.Log("on function sisällä1");
+        }
+        else if(clickCounter <= 3)
+        {
+            idle = false;
+            nosto_aloitus2 = false;
+            nosto = true;
+            Debug.Log("on function sisällä2");
+        }
+        else if(clickCounter <= 4)
+        {
+            idle = false;
+            nosto = false;
+            nosto2 = true;
+            Debug.Log("on function sisällä3");
+        }
+        else if(clickCounter <= 5)
+        {
+            nosto = false;
+            idle = false;
+            nosto2 = false;
+            nosto3 = true;
+            Debug.Log("on function sisällä4");
         }
     }
 
