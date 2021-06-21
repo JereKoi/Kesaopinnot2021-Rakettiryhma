65655139d6fc38185319e9ff2cfc8f4b0a8b097a
Jere <jerekoivisto7@gmail.com>
2021-06-21 09:27:05 +0300

    Save systeemin ohjelmointia -Jere

---
+++ Assets/Scripts/save/SaveManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager Instance { get; set; }
+    public savestate state;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+        Load();
+
+        Debug.Log(Helper.Serialize<savestate>(state));
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetString("save", Helper.Serialize<savestate>(state));
+    }
+
+    public void Load()
+    {
+        if (PlayerPrefs.HasKey("save"))
+        {
+            state = Helper.Deserialize<savestate>(PlayerPrefs.GetString("save"));
+        }
+        else
+        {
+            state = new savestate();
+            Save();
+            Debug.Log("No save file found, creating a new one");
+        }
+    }
+}
